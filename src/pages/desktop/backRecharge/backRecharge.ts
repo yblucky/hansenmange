5ec72b622f8cbeff32d1060ad456f3494570952c
zzwei <1510331524@qq.com>
2017-09-02 02:11:16 +0800

    OK

---
+++ src/pages/desktop/backRecharge/backRecharge.ts
@@ -0,0 +1,175 @@
+import { Component } from "@angular/core";
+import { HttpService } from "../../../providers/HttpService";
+import { Utils } from "../../../providers/Utils";
+import { ActivatedRoute } from '@angular/router';
+
+declare var $: any;
+declare var layer: any;
+var paraPage: any;
+
+@Component({
+    selector   : 'page-task',
+    templateUrl: './backRecharge.html',
+    styleUrls: ['./backRecharge.scss']
+})
+export class BackRechargePage {
+    find:any={
+      uid:"",
+      mobile:"",
+      nickName:"",
+      userName:"",
+      content:"",
+      startTime:"",
+      endTime:""
+    };
+    showTime:any = new Date();
+    path:string;
+    imgPath:string;
+    subData:any={};
+    constructor(private httpService:HttpService,private aroute:ActivatedRoute,private utils:Utils) {
+        this.aroute.params.subscribe( params  => {
+            this.showTime = new Date();
+        });
+        this.path = Utils.FILE_SERVE_URL;
+        this.loadData();
+        paraPage=this;
+    }
+
+    /**
+    * 加载数据
+    */
+    loadData(){
+        this.httpService.pagination({
+            url:'/task/list',
+            data:this.find
+        });
+    }
+
+    /**
+    * 弹出新增面板
+    */
+    showAddPanel(){
+        this.subData = {
+            name: '',
+            discription: '',
+            rewardNo:'',
+            link: '',
+            taskType: '',
+            remark: '',
+            status: '1'
+        };
+        layer.open({
+            title: "管理员充值",
+            btn: ["保存","退出"],
+            type: 1,
+            closeBtn: 0,
+            shade: 0,
+            fixed: true,
+            shadeClose: false,
+            resize: false,
+            area: ['500px','auto'],
+            content: $("#editPanel"),
+            yes: function(index:number){
+                if(paraPage.validator()){
+                    paraPage.httpService.post({
+                        url:'/task/add',
+                        data:paraPage.subData
+                    }).subscribe((data:any)=>{
+                        layer.closeAll();
+                        if(data.code==='0000'){
+                            //新增成功
+                           layer.msg(data.message,{
+                               icon: '1',
+                               time: 2000
+                           },function(){
+                               paraPage.loadData();
+                           });
+                        }else if(data.code==='9999'){
+                            Utils.show(data.message);
+                        }else{
+                            Utils.show("系统异常，请联系管理员");
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+
+      /**
+      * 弹出编辑面板
+      */
+      showEditPanel(item:any){
+          this.subData = Utils.copyObject(item);
+          layer.open({
+              title: "修改任务清单",
+              btn: ["保存","退出"],
+              type: 1,
+              closeBtn: 0,
+              shade: 0,
+              fixed: true,
+              shadeClose: false,
+              resize: false,
+              area: ['500px','auto'],
+              content: $("#editPanel"),
+              yes: function(index:number){
+                  if(paraPage.validator()){
+                      paraPage.httpService.post({
+                          url:'/task/update',
+                          data:paraPage.subData
+                      }).subscribe((data:any)=>{
+                          layer.closeAll();
+                          if(data.code==='0000'){
+                              //修改成功
+                             layer.msg(data.message,{
+                                 icon: '1',
+                                 time: 2000
+                             },function(){
+                                 paraPage.loadData();
+                             });
+                          }else if(data.code==='9999'){
+                              Utils.show(data.message);
+                          }else{
+                              Utils.show("系统异常，请联系管理员");
+                          }
+                      });
+                  }
+              }
+          });
+      }
+
+    showPath(img:any){
+        this.imgPath=img.imgPath;
+        layer.open({
+            title: "用户反馈图片",
+            type: 1,
+            closeBtn: 1,
+            shade: 0,
+            fixed: true,
+            shadeClose: true,
+            resize: false,
+            area: ['800px','700px'],
+            content: $("#editPanel")
+        });
+    }
+
+
+    validator(){
+        if(Utils.isEmpty(this.subData.name)){
+            layer.tips('任务标题不能为空', '#name',{tips: 1});
+            $("#name").focus();
+            return false;
+        }
+        if(Utils.isEmpty(this.subData.taskType)){
+            layer.tips('任务类型不能为空', '#taskType',{tips: 1});
+            $("#value").focus();
+            return false;
+        }
+        if(Utils.isEmpty(this.subData.link)){
+            layer.tips('任务链接不能为空', '#link',{tips: 1});
+            $("#title").focus();
+            return false;
+        }
+        return true;
+    }
+}
